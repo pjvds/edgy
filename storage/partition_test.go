fa5cf84d8688263b0dfec4f3ef635819c3c3d7f1
Pieter Joost van de Sande <pj@born2code.net>
2015-07-22 11:45:23 +0200

    test(storage): assert on entry details

--- storage/partition_test.go
+++ storage/partition_test.go
@@ -21,16 +21,19 @@ func TestAppendRoundtrip(t *testing.T) {
 	}
 	defer log.Close()
 
-	messages := NewMessageSet([]RawMessage{
+	err = log.Append(NewMessageSet([]RawMessage{
 		NewMessage(0, []byte("foo bar")),
 		NewMessage(1, []byte("baz")),
 		NewMessage(2, []byte("42")),
-	})
-
-	err = log.Append(messages)
+	}))
 	assert.Nil(t, err)
 
 	readResult, err := log.ReadFrom(MessageId(1), 1000)
 	assert.Nil(t, err)
 	assert.Len(t, readResult.entries, 2)
+
+	assert.Equal(t, MessageId(1), readResult.entries[0].Id)
+	assert.Equal(t, []byte("baz"), readResult.entries[0].Raw)
+	assert.Equal(t, MessageId(2), readResult.entries[1].Id)
+	assert.Equal(t, []byte("42"), readResult.entries[1].Raw)
 }
