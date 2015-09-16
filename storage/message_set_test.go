657bf1d201806c5c4b3778c052c227a16e0b65ef
Tomas Roos <ptomasroos@gmail.com>
2015-09-16 08:15:59 +0200

    Fixed failed test with MessageId vs uint64 assertion
    It seems that Assertion can't be done even though MessageId is a
    wrapped uint64

--- storage/message_set_test.go
+++ storage/message_set_test.go
@@ -17,6 +17,6 @@ func TestMessageSetAlign(t *testing.T) {
 	assert.Equal(t, MessageId(6), set.entries[0].Id)
 	assert.Equal(t, MessageId(7), set.entries[1].Id)
 
-	assert.Equal(t, MessageId(6), byteOrder.Uint64(set.buffer[set.entries[0].Offset+INDEX_ID:]))
-	assert.Equal(t, MessageId(7), byteOrder.Uint64(set.buffer[set.entries[1].Offset+INDEX_ID:]))
+	assert.Equal(t, MessageId(6), MessageId(byteOrder.Uint64(set.buffer[set.entries[0].Offset+INDEX_ID:])))
+	assert.Equal(t, MessageId(7), MessageId(byteOrder.Uint64(set.buffer[set.entries[1].Offset+INDEX_ID:])))
 }
