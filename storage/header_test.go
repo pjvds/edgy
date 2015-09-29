23ded2124a3a50c0fcb47febd6e54f3b5c35639f
Pieter Joost van de Sande <pj@born2code.net>
2015-09-29 17:25:29 +0200

    test(storage): add test for ReadHeaderUnsafe

--- storage/header_test.go
+++ storage/header_test.go
@@ -23,3 +23,20 @@ func TestReadHeader(t *testing.T) {
 	assert.Equal(t, header.ContentLength, result.ContentLength)
 	assert.Equal(t, header.ContentHash, result.ContentHash)
 }
+func TestReadHeaderUnsafe(t *testing.T) {
+	header := Header{
+		Magic:         START_VALUE,
+		MessageId:     MessageId(42),
+		ContentLength: 22,
+		ContentHash:   88,
+	}
+
+	buffer := make([]byte, HEADER_LENGTH)
+	header.Write(buffer)
+
+	result := ReadHeaderUnsafe(buffer)
+	assert.Equal(t, header.Magic, result.Magic)
+	assert.Equal(t, header.MessageId, result.MessageId)
+	assert.Equal(t, header.ContentLength, result.ContentLength)
+	assert.Equal(t, header.ContentHash, result.ContentHash)
+}
