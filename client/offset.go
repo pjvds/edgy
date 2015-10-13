56617da2626d83026dec64a7150a5687783cc649
Pieter Joost van de Sande <pj@born2code.net>
2015-10-13 13:18:50 +0200

    feat(client): add Offset

---
+++ client/offset.go
@@ -0,0 +1,35 @@
+package client
+
+import "github.com/pjvds/edgy/api"
+
+type Offset struct {
+	MessageId uint64 `json:"message_id"`
+	Position  int64  `json:"position"`
+	SegmentId uint64 `json:"segment_id"`
+}
+
+var OffsetBeginning = NewOffset(0, 0, 0)
+
+func NewOffset(messageId uint64, position int64, segmentId uint64) Offset {
+	return Offset{
+		MessageId: messageId,
+		Position:  position,
+		SegmentId: segmentId,
+	}
+}
+
+func (this Offset) toOffsetData() *api.OffsetData {
+	return &api.OffsetData{
+		MessageId:   this.MessageId,
+		EndPosition: this.Position,
+		SegmentId:   this.SegmentId,
+	}
+}
+
+func offsetFromOffsetData(offsetData *api.OffsetData) Offset {
+	return Offset{
+		MessageId: offsetData.MessageId,
+		Position:  offsetData.EndPosition,
+		SegmentId: offsetData.SegmentId,
+	}
+}
